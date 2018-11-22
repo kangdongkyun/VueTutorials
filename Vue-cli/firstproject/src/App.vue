869e0e9844c4c5d53775ce0f39343e54f326a190
kdk_kang_c <kdk_kang_c@naver.com>
2018-11-22 11:50:24 +0900

    add vue material

--- Vue-cli/firstproject/src/App.vue
+++ Vue-cli/firstproject/src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-    <router-link to="/">홈으로</router-link>
-    <router-link to="/ex1">예제1</router-link>
-    <router-link to="/ex2">예제2</router-link>
-    <router-link to="/ex3">예제3</router-link>
+    <md-button to="/" class="md-raised">홈으로</md-button>
+    <md-button to="/ex1" class="md-raised">예제1</md-button>
+    <md-button to="/ex2" class="md-raised">예제2</md-button>
+    <md-button to="/ex3" class="md-raised">예제3</md-button>
+
+
+    <!-- <router-link to="/"></router-link>
+    <router-link to="/ex1"></router-link>
+    <router-link to="/ex2"></router-link>
+    <router-link to="/ex3"></router-link> -->
     <router-view/>
   </div>
 </template>
     
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  
 }
 </script>
+ 
